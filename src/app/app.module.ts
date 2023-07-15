591c6024e84ee21a024ad5fc54ab3f879fec5341
ShibanshL <shibansh.w@gmail.com>
2023-07-15 12:57:24 +0530

    Tried adding svg, working on fixing that issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './myNewComponent/todos/todos.component';
+import { HeaderComponent } from './myNewComponent/header/header.component';
+import { BodyComponent } from './myNewComponent/body/body.component';
+import { FooterComponent } from './myNewComponent/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
